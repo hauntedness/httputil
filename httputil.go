49d19a5c96239bd4780725bc5e739ae383c84f9e
huantedness <keikoamo@163.com>
2023-08-15 20:10:15 +0800

    add RequestAndWriteTo for big content

--- httputil.go
+++ httputil.go
@@ -3,12 +3,14 @@ package httputil
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
-	"sync"
 )
+
+var ErrContenthCopyLength = errors.New("The number of bytes copied is not equal to response content length.")
 
 const (
 	DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 Edg/93.0.961.38"
@@ -19,7 +21,7 @@ type H map[string]string
 
 var client = &http.Client{}
 
-var retryQuata int = 1
+var maxRetries int = 1
 
 func Get(url string, headers H) (data []byte, err error) {
 	return Request(http.MethodGet, url, nil, headers)
@@ -64,7 +66,38 @@ func Json[T any](method string, url string, queryObject any, headers H) (value *
 	return t, nil
 }
 
-// send request and retry 3 times
+// RequestAndWriteTo send request and copy response body to dst
+// it return [ErrContenthCopyLength] if the copied number of bytes doesn't match ContentLength
+func RequestAndWriteTo(dst io.Writer, method string, url string, body io.Reader, headers H) (err error) {
+	// fmt.Println("going to: ", url)
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
+	// default setting
+	req.Header.Set("User-Agent", DefaultUserAgent)
+
+	// replace with input headers
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	written, err := io.Copy(dst, resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.ContentLength > -1 && resp.ContentLength != written {
+		return ErrContenthCopyLength
+	}
+	return nil
+}
+
+// send request and retry 3 times in default
 func Request(method string, url string, body io.Reader, headers H) (data []byte, err error) {
 	// fmt.Println("going to: ", url)
 	req, err := http.NewRequest(method, url, body)
@@ -81,7 +114,7 @@ func Request(method string, url string, body io.Reader, headers H) (data []byte,
 	}
 
 	var resp *http.Response
-	for retries := 0; retries < retryQuata; {
+	for retries := 0; retries < maxRetries; {
 		resp, err = client.Do(req)
 		if err != nil {
 			req.Body = io.NopCloser(body)
@@ -106,22 +139,18 @@ func Request(method string, url string, body io.Reader, headers H) (data []byte,
 }
 
 func SetRetry(retryTimes int) {
-	retryQuata = retryTimes + 1
+	maxRetries = retryTimes + 1
 }
 
-var once = sync.Once{}
-
 func SetProxy(proxyUrl string) {
-	once.Do(func() {
-		// Transport caches connections for future re-use,
-		// if call SetProxy too many times, the connection pool will no longer make sense
-		transport := &http.Transport{}
-		transport.Proxy = func(_ *http.Request) (*url.URL, error) {
-			if proxyUrl == "" {
-				proxyUrl = "http://127.0.0.1:58309"
-			}
-			return url.Parse(proxyUrl)
+	// Transport caches connections for future re-use,
+	// if call SetProxy too many times, the connection pool will no longer make sense
+	transport := &http.Transport{}
+	transport.Proxy = func(_ *http.Request) (*url.URL, error) {
+		if proxyUrl == "" {
+			proxyUrl = "http://127.0.0.1:58309"
 		}
-		client.Transport = transport
-	})
+		return url.Parse(proxyUrl)
+	}
+	client.Transport = transport
 }
